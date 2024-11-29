9c5ac547542ffdb7bded7ed1391db6466b27cb1c
Nathan <antnefedov33@gmail.com>
2024-11-29 13:34:12 +0300

    scss reset

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import App from '@/app/App';
+import '@/app/styles/index.scss';
 
 const container = document.getElementById('root');
 
 if (!container) {
-	throw new Error(
-		'No root container found. Failed to mount the react application'
-	);
+    throw new Error(
+        'No root container found. Failed to mount the react application',
+    );
 }
 
 const root = createRoot(container);
