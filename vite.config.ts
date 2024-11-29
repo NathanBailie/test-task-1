d282a45fbdce5a1697b3723dc7bc33457a8cb3e0
Nathan <antnefedov33@gmail.com>
2024-11-29 13:26:14 +0300

    added vite

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+    plugins: [svgr(), react()],
+    resolve: {
+        alias: [{ find: '@', replacement: '/src' }],
+    },
+    define: {
+        __IS_DEV__: JSON.stringify(true),
+    },
+});
